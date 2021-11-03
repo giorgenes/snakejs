f7bc03304b88d8247212bb171cf550d69133d203
Giorgenes Gelatti <giorgenes@gmail.com>
2021-11-03 13:17:55 +1100

    Intitialize webpack dev server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "<b>Hello!</b>";
+  element.innerHTML = "<b>Hello Snake World!</b>";
 
   return element;
 }
