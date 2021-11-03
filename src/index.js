b41bda2fb7355399c83dd91c3b75bff9dfad2659
Giorgenes Gelatti <giorgenes@gmail.com>
2021-11-03 16:44:52 +1100

    Initialize webgl and center the screen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+import Renderer from "./renderer";
+import './style.scss';
+
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "<b>Hello Snake World!</b>";
+  element.innerHTML = "<div class='container'><canvas id='canvas' width='640' height='480' /> </div>";
 
   return element;
 }
 
+function main() {
+  const canvas = document.querySelector("#canvas");
+  let renderer = new Renderer(canvas);
+  renderer.render({});
+}
+
 document.body.appendChild(component());
+
+window.onload = main;
+
---
+++ src/renderer.js
@@ -0,0 +1,21 @@
+export default class Renderer {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.gl = canvas.getContext("webgl");
+
+    // Only continue if WebGL is available and working
+    if (this.gl === null) {
+      alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+      return;
+    }
+
+    this.width = canvas.width;
+    this.height = canvas.height;
+  }
+
+  render(scene) {
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+  }
+}
